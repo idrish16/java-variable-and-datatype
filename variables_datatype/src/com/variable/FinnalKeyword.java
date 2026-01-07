793c7017c60038fe35107c0c7fab3b46104ebf98
idrish <goriirfan996@gmail.com>
2026-01-07 18:06:59 +0530

    about final keyword

---
+++ variables_datatype/src/com/variable/FinnalKeyword.java
@@ -0,0 +1,14 @@
+package com.variable;
+
+public class FinnalKeyword {
+//	In Java, the final keyword is used to restrict modification.
+//	Once something is declared final, it cannot be changed.
+	public static void main(String[] args) {
+		final int a =10;
+//		a=20; // it gives an error because we cannot modify a final variable values. 
+		System.out.println(a);
+	}
+	
+	
+
+}
